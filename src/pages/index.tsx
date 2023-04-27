aad83ba83228c32fb46d75bd31f4e6f819f122f9
SUBID DAS <itsmesubid@gmail.com>
2023-04-27 23:46:21 +0530

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Signup from "@/components/home/Signup";
 import styled from "styled-components";
 import sha256 from "crypto-js/sha256";
 import axios from "axios";
+import { authenticator } from "otplib";
 
 const Main = styled.main`
   min-height: 100vh;
@@ -21,15 +22,17 @@ const Home = () => {
   //   stat: 'Not_Ok',
   //   emsg: 'Invalid Input : Invalid OTP'
   // }
+  const secret = "2SKOG3TLZR4ZE7U4NKS2677X4KJ54W3D";
   const password = sha256("Shan@1234").toString();
   const userid = "FA97180";
   const apiSecret = "0d14c648394ca23897d46627fe7e08a8";
   const imei = "abc1234";
   const appKey = sha256(`${userid}|${apiSecret}`).toString();
+  const totp = authenticator.generate(secret);
   const data = {
     uid: userid,
     pwd: password,
-    factor2: "01011970",
+    factor2: totp,
     vc: "FA97180_U",
     appkey: appKey,
     apkversion: "1.0.0",
@@ -38,12 +41,11 @@ const Home = () => {
   };
 
   const jData = "jData=" + JSON.stringify(data);
-  console.log(jData);
+  console.log({ ...data });
   axios
     .post("https://api.shoonya.com/NorenWClientTP/QuickAuth/", jData)
-    .then((res) => {
-      console.log(`Status: ${res.status}`);
-      console.log("Body: ", res.data);
+    .then(({ data }) => {
+      console.log(data);
     })
     .catch((err) => {
       console.error(err);
